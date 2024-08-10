7a445d710776018a380c7392cb69411da65fecaf
KyrA-github <korenovsostav@gmail.com>
2024-08-10 03:01:21 +0300

    optimization

--- include/library.h
+++ include/library.h
@@ -4,9 +4,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 extern "C" __declspec(dllexport) void initLib(sf::RenderWindow& window);
 extern "C" __declspec(dllexport) void menuLib(sf::RenderWindow& window, int& menuStatus);
 void SettingLogic(sf::RenderWindow& window, int& menuStatus);
+float percentage(float percent, unsigned int num);
+unsigned int textSizeOptimization(sf::Vector2u currentSize, float textSize);
+
 
 #endif // MOD_MENU_H
--- src/library.cpp
+++ src/library.cpp
@@ -1,5 +1,13 @@
 #include "../include/library.h"
 
+float referenceWidth = 1280.0f;
+float referenceHeight = 800.0f;
+float referenceTextSize = 30.0f;
+
+// Текущие размеры окна
+float currentWidth;
+float currentHeight;
+
 int SettingsStatus = 0;
 
 bool exitmenu = false;
@@ -17,48 +25,58 @@ sf::Color colorNormal(255, 255, 255); // Цвет по умолчанию
 sf::Text settings[5];
 sf::Text settingsGraphics[5];
 
+sf::Vector2u windowSize;
+
 
 extern "C" __declspec(dllexport) void initLib(sf::RenderWindow& window) {
     if (!font.loadFromFile("Dependency/font.otf")) {
-        std::cerr << "Error loading font" << std::endl;
+        std::cerr << "ERROR: Failed to initialize menu. Failed to load font" << std::endl;
         window.close();
         return;
     }
 
+    windowSize = window.getSize();
+
+    float xText = percentage(4.69f, windowSize.x);
+
     CreateWorldText.setFont(font);
     CreateWorldText.setString("Create World");
-    CreateWorldText.setCharacterSize(40);
+    CreateWorldText.setCharacterSize(textSizeOptimization(windowSize, 40.0f));
     CreateWorldText.setFillColor(sf::Color::White);
-    CreateWorldText.setPosition(60, 400);
+    CreateWorldText.setPosition(xText, percentage(50.0f, windowSize.y));
 
     SettingsText.setFont(font);
     SettingsText.setString("Settings");
-    SettingsText.setCharacterSize(40);
+    SettingsText.setCharacterSize(textSizeOptimization(windowSize, 40.0f));
     SettingsText.setFillColor(sf::Color::White);
-    SettingsText.setPosition(60, 500);
+    SettingsText.setPosition(xText, percentage(62.5f, windowSize.y));
 
     ModNameText.setFont(font);
     ModNameText.setString("Mods");
-    ModNameText.setCharacterSize(40);
+    ModNameText.setCharacterSize(textSizeOptimization(windowSize, 40.0f));
     ModNameText.setFillColor(sf::Color::White);
-    ModNameText.setPosition(60, 600);
+    ModNameText.setPosition(xText, percentage(75.0f, windowSize.y));
 
     ExitText.setFont(font);
     ExitText.setString("Exit");
-    ExitText.setCharacterSize(40);
+    ExitText.setCharacterSize(textSizeOptimization(windowSize, 40.0f));
     ExitText.setFillColor(sf::Color::White);
-    ExitText.setPosition(60, 700);
+    ExitText.setPosition(xText, percentage(87.5f, windowSize.y));
 
     GameNameText.setFont(font);
     GameNameText.setString("Pixel-Warriors");
-    GameNameText.setCharacterSize(55);
+    GameNameText.setCharacterSize(textSizeOptimization(windowSize, 60.0f));
     GameNameText.setFillColor(sf::Color(204, 155, 248));
-    GameNameText.setPosition(700, 50);
+    GameNameText.setPosition(percentage(54.69, windowSize.x), 50);
 
     for (int i = 0; i < 5; i++) {
         settings[i].setFont(font);
-        settings[i].setCharacterSize(30);
+        settings[i].setCharacterSize(textSizeOptimization(windowSize, 30.0f));
         settings[i].setFillColor(sf::Color::White);
+
+        settingsGraphics[i].setFont(font);
+        settingsGraphics[i].setCharacterSize(textSizeOptimization(windowSize, 30.0f));
+        settingsGraphics[i].setFillColor(sf::Color::White);
     }
 
     settings[0].setString("Exit");
@@ -69,13 +87,13 @@ extern "C" __declspec(dllexport) void initLib(sf::RenderWindow& window) {
     settings[3].setCharacterSize(40);
     settings[4].setString("Keys");
 
-    settings[0].setPosition(60, 700);
-    settings[1].setPosition(60, 400);
-    settings[2].setPosition(60, 300);
-    settings[3].setPosition(200, 700);
-    settings[4].setPosition(60, 500);
+    settings[0].setPosition(xText, percentage(87.5f, windowSize.y));
+    settings[1].setPosition(xText, percentage(50.0f, windowSize.y));
+    settings[2].setPosition(xText, percentage(37.5f, windowSize.y));
+    settings[3].setPosition(percentage(15.63f, windowSize.x), percentage(87.5f, windowSize.y));
+    settings[4].setPosition(xText, percentage(62.5f, windowSize.y));
 
-    std::cout << "[INFO]: menu initialized is successful" << std::endl;
+    std::cout << "INFO: menu initialized is successful" << std::endl;
 }
 
 extern "C" __declspec(dllexport) void menuLib(sf::RenderWindow& window, int& menuStatus) {
@@ -84,6 +102,7 @@ extern "C" __declspec(dllexport) void menuLib(sf::RenderWindow& window, int& men
     if (!sf::Mouse::isButtonPressed(sf::Mouse::Left))
         exitmenu = true;
     if (menuStatus == 0) {
+
         //Create World
         if (CreateWorldText.getGlobalBounds().contains(mousePosition.x, mousePosition.y)) {
             CreateWorldText.setFillColor(colorHover);
@@ -189,3 +208,13 @@ void SettingLogic(sf::RenderWindow& window, int& menuStatus) {
     }
     window.draw(GameNameText);
 }
+
+
+float percentage(float percent, unsigned int num) {
+    return (percent / 100) * num;
+}
+
+unsigned int textSizeOptimization(sf::Vector2u currentSize, float textSize) {
+    return int(textSize * (std::sqrt(std::pow((currentSize.x / 1280.0f), 2) + std::pow((currentSize.y / 800.0f), 2)) / std::sqrt(2)));
+}
+
