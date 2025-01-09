eb99d805c5755c9280000e6069e17fd65ecd232e
KyrA-github <korenovsostav@gmail.com>
2025-01-09 20:50:42 +0300

    sf

--- include/library.h
+++ include/library.h
@@ -11,10 +11,10 @@ extern "C" __declspec(dllexport) void menuLib(sf::RenderWindow& window, int& men
 void SettingLogic(sf::RenderWindow& window, int& menuStatus, std::map<std::string, int>& settingsMap);
 void CreateWorld(sf::RenderWindow& window, int& menuStatus, const int& mouseEventWheel, const float& time, std::map<std::string, std::string>& map);
 void ModsLogic(sf::RenderWindow& window, int& menuStatus, std::map<std::string, std::string>& ModsMap);
-// float percentage(float percent, unsigned int num);
-// float percentage1280(float percent, unsigned int num);
-// float percentage800(float percent, unsigned int num);
-// unsigned int textSizeOptimization(sf::Vector2u currentSize, float textSize);
+float percentage(float percent, unsigned int num);
+float percentage1280(float percent, unsigned int num);
+float percentage800(float percent, unsigned int num);
+unsigned int textSizeOptimization(sf::Vector2u currentSize, float textSize);
 // bool collisionMouse(const sf::Vector2f& mousePosition, sf::Vector2f shapeSize, sf::Vector2f shapePosition);
 
 #endif
